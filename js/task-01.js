9deb0f069a2a980c4600d6cb81941c3b41406e6f
SofiaPogranychna <126394892+SofiaPogranychna@users.noreply.github.com>
2023-07-27 14:54:59 -0700

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,8 @@ console.log(`Number of categories: ${ulCategories.children.length}`);
 
 [...ulCategories.children].forEach((item) => {
     
-    const itemTitle = item.querySelector("h2");
-    const itemNumber = item.querySelectorAll("li");
+    const itemTitle = item.firstElementChild;
+    const itemNumber = item.lastElementChild.children;
 
 console.log(`Category: ${itemTitle.textContent}`);
 console.log(`Elements: ${itemNumber.length}`);
